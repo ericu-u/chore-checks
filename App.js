8bcd7db2e312c55edcd5953be226a4596700aaa0
Eric Wang <wang.eric1115@gmail.com>
2021-07-15 17:39:00 -0700

    adjusted colors to match mock design

--- App.js
+++ App.js
@@ -2,30 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
+
+// Add navigation header section (diff color than page)
+
 export default function App() {
   const handleTap = () => console.log('i suck pp');
 
   return (
     <SafeAreaView style={styles.container}>
 
-      <View style={{ flex: 0.25, backgroundColor: "dodgerblue" }}></View>
-      <View style={{ flex: 1.25, backgroundColor: "dodgerblue" }}>
+      <View style={{ flex: 0.25 }}></View>
+      <View style={{ flex: 1.25 }}>
 
         <Image source={require("./app/assets/oatmeal_logo.jpg")} style={styles.image}/>
 
       </View>
 
-      <View style={styles.container, { flex: 0.5, backgroundColor: "gold", alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container, { flex: 0.5, alignItems: 'center', justifyContent: 'center' }}>
 
-        <Text style={styles.titleText} onPress={handleTap}>Chore Check</Text>
+        <Text style={styles.titleText} onPress={handleTap}>Chore Checks</Text>
 
       </View>
 
-      <View style={{ flex: 2, backgroundColor: "dodgerblue", alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={styles.text} onPress={handleTap}>Google Sign-in here</Text>
       </View>
 
-      <View style={{ flex: 0.5, backgroundColor: "gold", alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 0.5, alignItems: 'center', justifyContent: 'center' }}>
 
         <Text style={styles.text} onPress={handleTap}>Fun Fact: Quaker Oats was founded in 1877!</Text>
 
@@ -39,7 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a6ddf1',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -48,7 +51,8 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   },
   titleText: {
-    fontSize: 40
+    fontSize: 40,
+    color: 'white'
   },
   text: {
     fontSize: 20
