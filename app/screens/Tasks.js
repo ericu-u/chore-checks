a6e0bfd566ba8e9079f24a38f101384caf7a562f
Eric Wang <wang.eric1115@gmail.com>
2021-07-19 01:36:18 -0700

    added statusheader view

--- app/screens/Tasks.js
+++ app/screens/Tasks.js
@@ -5,23 +5,40 @@ import { Header } from 'react-native-elements';
 
 function Tasks(props) {
     return (
-        <View style={styles.background}>
+        <View style={ styles.background }>
             <Header
-                style={styles.background}
                 leftComponent={{ icon: 'menu', color: '#fff', iconStyle: { color: '#fff' } }}
                 centerComponent={{ text: 'Tasks', style: { color: '#fff' } }}
             />
+
+            <View style={ styles.statusHeader }>
+                <Text style={{ textAlign: 'center', marginTop: 20 }}>Active</Text>
+            </View>
+
+            <View style={ styles.individualTask }>
+                <View style={ styles.individualTask }></View>
+            </View>
+
+
         </View>
     );
 }
-
-//#52a7d1
 
 const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: '#52a7d1',
     },
+    statusHeader: {
+        flex: 1,
+        backgroundColor: '#52a7d1',
+    },
+    individualTask: {
+        flex: 1,
+        height: 100,
+        backgroundColor: '#b7daeb',
+        justifyContent: 'flex-start',
+    }
 })
 
 export default Tasks;
