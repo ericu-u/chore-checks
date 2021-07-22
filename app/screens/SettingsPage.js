c2408cc9f9e3e008a307d00b58b6c949eb42fc00
Eric Wang <wang.eric1115@gmail.com>
2021-07-21 18:44:59 -0700

    redid some deletions from last commit

---
+++ app/screens/SettingsPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SettingsPage(props) {
+    return (
+        <ImageBackground
+            style={{flex:1}}
+            source={require("../assets/background-gradient.jpg")}
+        >
+
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default SettingsPage; 
