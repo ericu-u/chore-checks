4454666d54f12ea9e753921c938e712e1a94f3f8
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2021-08-09 11:54:50 -0700

    Merged with all other branches

--- app/screens/HouseholdPage.js
+++ app/screens/HouseholdPage.js
@@ -1,151 +1,234 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-import { SafeAreaView, View, Text, StyleSheet, Image, StatusBar, ImageBackground, Button, TouchableHighlight } from 'react-native';
-import { Header, withTheme } from 'react-native-elements';
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+  TouchableHighlight,
+  Button,
+  FlatList,
+  StatusBar,
+  SectionList,
+  Image,
+} from "react-native";
+import config from "../../config";
+import * as firebase from "firebase";
+import "firebase/firestore";
+import Household from "../classes/household";
+import Task from "../classes/task";
+import { Header } from "react-native-elements";
+import Person from "../classes/person";
 
+export default class HouseholdPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [], // List of Person objects
+      unsubscribe: null, // Firebase subscription. Calling this method will mean we stop listening to firebase for updates whenever the database changes
+      tasks: [], // 2D array. The i-th position of tasks contains all the tasks the person in the i-th person on people accomplished.
+      unsubscribe2: null,
+    };
+  }
 
-function onPressButton() {
-    alert('Change status'); // TODO: Alert and/or Button to be replaced
-}
+  componentDidMount() {
+    // This method is called when the component is first rendered
+    // Set up firebase stuff
+    var firebaseConfig = {
+      apiKey: config.FIREBASE_KEY,
+      authDomain: "chores-97427.firebaseapp.com",
+      projectId: "chores-97427",
+      storageBucket: "chores-97427.appspot.com",
+      messagingSenderId: "409040868260",
+      appId: "1:409040868260:web:7b6d1f00e29554af802731",
+      measurementId: "G-8D3XVC7R9T",
+    };
 
-function HouseholdPage(props) {
-    return ( //replace all margins/paddings with relative positioning based on device
-
-        
-        <ImageBackground
-            style={{flex:1}}
-            source={require("../assets/background-gradient.jpg")}
-        >
-
-            <View style={{flexDirection: 'column', alignItems: 'center'}}>
-                <View style={ styles.statusHeader }>
-                    <Text style={ styles.statusHeaderText }>Sort By Points</Text>
-                </View>
-            </View>
-            
-            <ScrollView>
-            <View style={ styles.individualGroup }>
-                <View style={ styles.individualPoints }>
-                    <View style ={styles.avatarPosition}>
-                        <Image source={require("../assets/standard-account90.png")} />
-                    </View>
-                    <View style={styles.topRight }>
-                        <Image source={require("../assets/gold-trophy.png")} /> 
-                    </View>
-                    <Text style={{fontSize:18, flex:1, textAlign: 'center',paddingBottom: "3%",}}>Username</Text>
-                    <Text style={{ textAlign: 'center'}}>Points: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Tasks Completed: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Completion Rate: Placeholder</Text>
-                </View>
-            </View>
-
-            <View style={ styles.individualGroup }>
-                <View style={ styles.individualPoints }>
-                <View style ={styles.avatarPosition}>
-                        <Image source={require("../assets/standard-account90.png")} />
-                    </View>
-                <View style={styles.topRight }>
-                        <Image source={require("../assets/silver-trophy.png")} /> 
-                    </View>
-                    <Text style={{fontSize:18, flex:1, textAlign: 'center',paddingBottom: "3%",}}>Username</Text>
-                    <Text style={{ textAlign: 'center'}}>Points: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Tasks Completed: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Completion Rate: Placeholder</Text>
-                    
-                </View>
-            </View>
-
-            <View style={ styles.individualGroup }>
-                <View style={ styles.individualPoints }>
-                <View style ={styles.avatarPosition}>
-                        <Image source={require("../assets/standard-account90.png")} />
-                    </View>
-                <View style={styles.topRight }>
-                        <Image source={require("../assets/bronze-trophy.png")} /> 
-                    </View>
-                    <Text style={{fontSize:18, flex:1, textAlign: 'center',paddingBottom: "3%",}}>Username</Text>
-                    <Text style={{ textAlign: 'center'}}>Points: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Tasks Completed: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Completion Rate: Placeholder</Text>
-                </View>
-            </View>
-
-            <View style={ styles.individualGroup }>
-                <View style={ styles.individualPoints }>
-                    <View style ={styles.avatarPosition}>
-                        <Image source={require("../assets/standard-account90.png")} />
-                    </View>
-                    <Text style={{fontSize:18, flex:1, textAlign: 'center',paddingBottom: "3%",}}>Username</Text>
-                    <Text style={{ textAlign: 'center'}}>Points: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Tasks Completed: Placeholder</Text>
-                    <Text style={{ textAlign: 'center',}}>Completion Rate: Placeholder</Text>
-                </View>
-            </View>
-            </ScrollView>
-            
-
-  
-        </ImageBackground>
-    );
-}
-const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        backgroundColor: '#52a7d1',
-    },
-    statusHeader: {
-        //height: "30%", //replace with relative positioning based on device
-        justifyContent: 'center',
-        marginTop: "6%",
-        marginBottom: "6%",
-    },
-    statusHeaderText: {
-        fontSize: 25,
-        justifyContent: 'center',
-        textAlign: 'center',
-    },
-    individualGroup: {
-        justifyContent: 'space-evenly',
-        //alignItems: "center",
-        // set a max height (maybe)
-    },
-    individualPoints: {
-        //height: "100%", //replace with relative positioning based on device
-        // set alternating background color
-        justifyContent: 'flex-start',
-        // set PROPER borders
-        flexDirection: 'column',
-        alignContent: 'center',
-        alignItems: 'flex-start',
-        backgroundColor: 'white',
-        paddingLeft: "35%",
-        marginLeft: "7%",
-        marginRight: "7%",
-        paddingTop: "7%",
-        paddingBottom: "7%",
-        //justifyContent: 'space-evenly',  
-        marginBottom: "4%",    
-        borderRadius: 6,  
-        overflow: 'hidden',
-    },
-    
-    topRight: {
-        position: 'absolute',
-        paddingLeft:"112%",
-        paddingTop: "3%",
-        //height: "1%",
-        //width: "1%",
-        ///margin: 'auto',
-    },
-
-    avatarPosition: {
-        position: 'absolute',
-        //height: "30%",
-        //width: "30%",
-        //margin: 'auto',
-        paddingLeft: '20%',
-        paddingTop: '15%',
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    } else {
+      firebase.app();
     }
-})
+    const db = firebase.firestore();
 
-export default HouseholdPage;
+    // Firestore subscription. Listens to database for changes.
+    var unsub = db
+      .collection("/houses/h38219/People")
+      .orderBy("points", "desc")
+      .withConverter(Person.personConverter)
+      .onSnapshot((querySnapshot) => {
+        // Whenever there is a change in firestore, this method runs
+        var tempPpl = []; // This temp array will store all the People from firestore
+        var tempTasks = [];
+        querySnapshot.forEach((doc) => {
+          tempPpl.push(doc.data());
+          tempTasks.push([]);
+        });
+        console.log("updated ppl");
+        this.setState({ people: tempPpl }); // Makes the state.people equal tempPpl
+        this.setState({ tasks: tempTasks });
+
+        const setTasks = async () => {
+          tempTasks = [];
+          this.state.people.forEach(async (p) => {
+            // var temtem = await p.getTasks();
+            // console.log(p.personID, temtem);
+            // tempTasks.push();
+            // tempTasks.push(temtem);
+            // this.setState({ tasks: tempTasks });
+            // console.log("tasks", this.state.tasks);
+            // console.log("people", this.state.people);
+            tempTasks.push([]);
+          });
+        };
+        setTasks();
+      });
+
+    this.setState({ unsubscribe: unsub }); // We save our subscription so we can end it later
+  }
+
+  componentWillUnmount() {
+    // This method runs whenever we stop rendering the component
+    this.state.unsubscribe(); // We end the subscription here so we don't waste resources
+  }
+  render() {
+    return (
+      //replace all margins/paddings with relative positioning based on device
+
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("../assets/background-gradient.jpg")}
+      >
+        <View style={{ flexDirection: "column", alignItems: "center" }}>
+          <View style={styles.statusHeader}>
+            <Text style={styles.statusHeaderText}>Sort By Points</Text>
+          </View>
+        </View>
+
+        <SectionList
+          sections={[{ title: "Household", data: this.state.people }]}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({ item, index }) => (
+            <Item title={item} index={index} tasksDone={this.state.tasks} />
+          )}
+          renderSectionHeader={({ section: { title } }) => <View></View>}
+        />
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: "#52a7d1",
+  },
+  statusHeader: {
+    //height: "30%", //replace with relative positioning based on device
+    justifyContent: "center",
+    marginTop: "6%",
+    marginBottom: "6%",
+  },
+  statusHeaderText: {
+    fontSize: 25,
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  individualGroup: {
+    justifyContent: "space-evenly",
+    //alignItems: "center",
+    // set a max height (maybe)
+  },
+  individualPoints: {
+    //height: "100%", //replace with relative positioning based on device
+    // set alternating background color
+    justifyContent: "flex-start",
+    // set PROPER borders
+    flexDirection: "column",
+    alignContent: "center",
+    alignItems: "flex-start",
+    backgroundColor: "white",
+    paddingLeft: "35%",
+    marginLeft: "7%",
+    marginRight: "7%",
+    paddingTop: "7%",
+    paddingBottom: "7%",
+    //justifyContent: 'space-evenly',
+    marginBottom: "4%",
+    borderRadius: 6,
+    overflow: "hidden",
+  },
+
+  topRight: {
+    position: "absolute",
+    paddingLeft: "112%",
+    paddingTop: "3%",
+    //height: "1%",
+    //width: "1%",
+    ///margin: 'auto',
+  },
+
+  avatarPosition: {
+    position: "absolute",
+    //height: "30%",
+    //width: "30%",
+    //margin: 'auto',
+    paddingLeft: "20%",
+    paddingTop: "15%",
+  },
+});
+
+function Trophy(index) {
+  if (index.index === 0) {
+    return <Image source={require("../assets/gold-trophy.png")} />;
+  } else if (index.index === 1) {
+    return <Image source={require("../assets/silver-trophy.png")} />;
+  } else if (index.index === 2) {
+    return <Image source={require("../assets/bronze-trophy.png")} />;
+  }
+  return <View></View>;
+}
+
+function NumTasks(index) {
+  try {
+    return (
+      <Text style={{ textAlign: "center" }}>
+        Tasks Completed: {index.tasksDone[index.index].length}
+      </Text>
+    );
+  } catch {
+    return <Text style={{ textAlign: "center" }}>Tasks Completed: 0</Text>;
+  }
+}
+
+const Item = ({ title, index, tasksDone }) => (
+  <View style={styles.individualGroup}>
+    <View style={styles.individualPoints}>
+      <View style={styles.avatarPosition}>
+        <Image source={require("../assets/standard-account90.png")} />
+      </View>
+      <View style={styles.topRight}>
+        <Trophy index={index}></Trophy>
+      </View>
+      <Text
+        style={{
+          fontSize: 18,
+          flex: 1,
+          textAlign: "center",
+          paddingBottom: "3%",
+        }}
+      >
+        {title.name}
+      </Text>
+      <Text style={{ textAlign: "center" }}>Points: {title.points}</Text>
+      <Text style={{ textAlign: "center" }}>
+        Tasks Completed: {title.tasksCompleted}
+      </Text>
+
+      <Text style={{ textAlign: "center" }}>
+        Completion Rate: {title.successRate}
+      </Text>
+    </View>
+  </View>
+);
--- app/screens/TasksPage.js
+++ app/screens/TasksPage.js
@@ -1,207 +1,348 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
-  SafeAreaView,
+  Button,
   View,
   Text,
   StyleSheet,
   ImageBackground,
+  Image,
   TouchableHighlight,
-  Button,
+  Modal,
   StatusBar,
   SectionList,
+  Pressable,
+  FlatList,
+  TouchableOpacity,
+  clickHandler,
 } from "react-native";
-import { Header, Overlay } from "react-native-elements";
-import { useNavigation } from "@react-navigation/native";
-import { StackHeaderLeftButtonProps } from "@react-navigation/stack";
-import { MaterialIcons } from "@expo/vector-icons";
 import config from "../../config";
 import * as firebase from "firebase";
 import "firebase/firestore";
 import Household from "../classes/household";
 import Task from "../classes/task";
-import Test from "./Test";
+import { Header } from "react-native-elements";
+import _, { map } from "underscore";
+import { FAB } from "react-native-paper";
 
-function TasksPage(props) {
-  //const taskGroupHeight;
-
-  var [reload, setReload] = useState(false);
-  var [stateTasks, setTasks] = useState([]);
-
-  var firebaseConfig = {
-    apiKey: config.FIREBASE_KEY,
-    authDomain: "chores-97427.firebaseapp.com",
-    projectId: "chores-97427",
-    storageBucket: "chores-97427.appspot.com",
-    messagingSenderId: "409040868260",
-    appId: "1:409040868260:web:7b6d1f00e29554af802731",
-    measurementId: "G-8D3XVC7R9T",
-  };
-
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  } else {
-    firebase.app();
+export class TasksPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [], // List of task objects
+      unsubscribe: null, // Firebase subscription. Calling this method will mean we stop listening to firebase for updates whenever the database changes
+      sectionedTasks: [],
+      modalVisible: true,
+    };
   }
 
-  useEffect(() => {
-    console.log("tasks are:", tasks);
-  }, [tasks]);
+  componentDidMount() {
+    // This method is called when the component is first rendered
+    // Set up firebase stuff
+    var firebaseConfig = {
+      apiKey: config.FIREBASE_KEY,
+      authDomain: "chores-97427.firebaseapp.com",
+      projectId: "chores-97427",
+      storageBucket: "chores-97427.appspot.com",
+      messagingSenderId: "409040868260",
+      appId: "1:409040868260:web:7b6d1f00e29554af802731",
+      measurementId: "G-8D3XVC7R9T",
+    };
 
-  const db = firebase.firestore();
-  // TODO householdID of the user should be stored in the householdID variable. For now, I will just make it the only household we have
-  const householdID = "h38219";
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    } else {
+      firebase.app();
+    }
+    const db = firebase.firestore();
 
-  var tasks = [];
-
-  console.log("REFRESHED");
-  db.collection("/houses/h38219/Tasks")
-    .withConverter(Task.taskConverter)
-    .onSnapshot((querySnapshot) => {
-      var tempTasks = [];
-      querySnapshot.forEach((doc) => {
-        //console.log(doc.data());
-        tempTasks.push(doc.data());
+    // Firestore subscription. Listens to database for changes.
+    var unsub = db
+      .collection("/houses/h38219/Tasks")
+      .withConverter(Task.taskConverter)
+      .onSnapshot((querySnapshot) => {
+        // Whenever there is a change in firestore, this method runs
+        var tempTasks = []; // This temp array will store all the Tasks from firestore
+        querySnapshot.forEach((doc) => {
+          tempTasks.push(doc.data());
+        });
+        console.log("updated tasks");
+        this.setState({ tasks: tempTasks }); // Makes the state.tasks equal tempTasks
+        var activeTasks = _.where(this.state.tasks, { completed: null });
+        var completedTasks = _.reject(this.state.tasks, function (task) {
+          return task.completed != null;
+        });
+        var sections = [
+          {
+            title: "Active",
+            data: activeTasks,
+          },
+          {
+            title: "Inactive",
+            data: completedTasks,
+          },
+        ];
+        this.setState({ sectionedTasks: sections });
       });
-      tasks = tempTasks;
-      // setTasks(tasks);
-    });
-
-  var docRef = db.doc("/houses/h38219");
-
-  (async () => {
-    // console.log((await docRef.get()).data());
-  })();
-
-  function onPressButton() {
-    alert("Change status"); // TODO: Alert and/or Button to be replaced
+    this.setState({ unsubscribe: unsub }); // We save our subscription so we can end it later
+  }
+  componentWillUnmount() {
+    // This method runs whenever we stop rendering the component
+    this.state.unsubscribe(); // We end the subscription here so we don't waste resources
   }
 
-  const [visible, setVisible] = useState(false);
-
-  const toggleOverlay = () => {
-    setVisible(!visible);
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
   };
 
-  // name, deadline, points, repeat, description, householdID
-  var testTaskOne = new Task(
-    "Do the Dishes",
-    Date.now() + 99999999,
-    10,
-    null,
-    "Wash and rinse dishes in the sink.",
-    12345
-  );
+  render() {
+    // Returns what we want the user to see
+    const { modalVisible } = this.state;
+    return (
+      // TODO: replace all margins/paddings with relative positioning based on device
 
-  var testTaskTwo = new Task(
-    "Walk the Dog",
-    Date.now() + 99999999,
-    10,
-    null,
-    "Take Freddy around the neighborhood.",
-    12345
-  );
-
-  var testData = [
-    {
-      title: "Active",
-      data: [testTaskOne, testTaskTwo],
-    },
-    {
-      title: "Inactive",
-      data: [],
-    },
-  ];
-
-  const Item = ({ task }) => (
-    <View style={styles.item}>
-      <Text style={{ flex: 1, backgroundColor: "pink", textAlign: "center" }}>
-        Points Placeholder
-      </Text>
-      <Text
-        style={{ flex: 2, backgroundColor: "aqua", textAlign: "center" }}
-        onPress={toggleOverlay}
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("../assets/background-gradient.jpg")}
       >
-        {task.name} {"\n"} Due Date: {new Date(task.deadline).getMonth()} /{" "}
-        {new Date(task.deadline).getDate()}{" "}
-        {/* Add if statement to display time if less than 24 hours */}
-      </Text>
-      <View style={{ flex: 1, backgroundColor: "gold" }}>
-        <Button title="Button" onPress={onPressButton}></Button>
-      </View>
-    </View>
-  );
-
-  return (
-    // TODO: replace all margins/paddings with relative positioning based on device
-
-    <ImageBackground
-      style={{ flex: 1 }}
-      source={require("../assets/background-gradient.jpg")}
-    >
-
-      <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
-        <Text>Hello testing</Text>
-      </Overlay>
-
-      <SectionList
-        sections={tasks}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({ item }) => <Item task={item} />}
-        renderSectionHeader={({ section: { title } }) => (
-          <View style={styles.statusHeader}>
-            <View
-              style={{
-                flex: 1,
-                height: 1,
-                backgroundColor: "black",
-                marginRight: "5%",
-                marginLeft: "10%",
-              }}
-            />
-            <View style={{ justifyContent: "center" }}>
-              <Text style={styles.statusHeaderText}>{title}</Text>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            this.setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalHeader}>Task Name Here</Text>
+              <FlatList
+                data={[
+                  { key: "Task Property 1" },
+                  { key: "Task Property 2" },
+                  { key: "Task Property 3" },
+                  { key: "Task Property 4" },
+                  { key: "Task Property 5" },
+                  { key: "Task Property 6" },
+                ]}
+                renderItem={({ item }) => (
+                  <Text style={{ fontSize: 15, textAlign: "left", margin: 5 }}>
+                    {item.key}
+                  </Text>
+                )}
+              />
+              <View style={{ position: "absolute", bottom: 10 }}>
+                <Button
+                  style={styles.modalButton}
+                  onPress={() => this.setModalVisible(!modalVisible)}
+                  title="Close"
+                />
+              </View>
             </View>
-            <View
-              style={{
-                flex: 1,
-                height: 1,
-                backgroundColor: "black",
-                marginRight: "10%",
-                marginLeft: "5%",
-              }}
-            />
           </View>
-        )}
+        </Modal>
+
+        <SectionList
+          sections={this.state.sectionedTasks}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({ item }) => <Item task={item} temp={this.state} />}
+          renderSectionHeader={({ section: { title } }) => (
+            <View style={styles.statusHeader}>
+              <View
+                style={{
+                  flex: 1,
+                  height: 1,
+                  backgroundColor: "black",
+                  marginRight: "5%",
+                  marginLeft: "10%",
+                }}
+              />
+              <View style={{ justifyContent: "center" }}>
+                <Text style={styles.statusHeaderText}>{title}</Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  height: 1,
+                  backgroundColor: "black",
+                  marginRight: "10%",
+                  marginLeft: "5%",
+                }}
+              />
+            </View>
+          )}
+        />
+
+        <AddButton></AddButton>
+      </ImageBackground>
+    );
+  }
+}
+
+const AddButton = () => (
+  <FAB
+    style={styles.fab}
+    icon="plus"
+    color="white"
+    onPress={() => console.log("Pressed")}
+  />
+);
+
+const Item = ({ task, temp }) => (
+  <View style={styles.item}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: 20,
+      }}
+    >
+      <Image
+        source={require("../assets/hexagon.png")}
+        style={{ height: 50, width: 50 }}
       />
-    </ImageBackground>
-  );
+      <Text style={{ position: "absolute" }}>{task.points}</Text>
+    </View>
+
+    <View style={{ flex: 4, marginLeft: 30, flexDirection: "column" }}>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={console.log(temp.modalVisible)}
+      >
+        <Text style={{ fontSize: 18 }}>{task.name}</Text>
+      </Pressable>
+
+      <Text style={{ fontSize: 13, color: "#db1414" }}>
+        Due Date: {new Date(task.deadline).getMonth()} /{" "}
+        {new Date(task.deadline).getDate()}{" "}
+        {/* TODO: Add if statement to display time if less than 24 hours */}
+      </Text>
+    </View>
+
+    <View style={{ flex: 2, justifyContent: "center" }}>
+      <TouchableHighlight
+        style={styles.claimStatus}
+        onPress={onPressButton}
+        underlayColor="#96c7eb"
+      >
+        <Text style={styles.statusText}>Taken</Text>
+      </TouchableHighlight>
+    </View>
+  </View>
+);
+
+function onPressButton() {
+  alert("Status has been changed."); // TODO: Alert and/or Button to be replaced
 }
 
 const styles = StyleSheet.create({
-  statusHeader: {
-    height: 80, // TODO: replace with relative positioning based on device
+  centeredView: {
+    flex: 1,
     justifyContent: "center",
-    borderBottomWidth: 0.5,
-    borderTopWidth: 0.5,
-    flexDirection: "row",
     alignItems: "center",
+    marginTop: 22,
   },
-  statusHeaderText: {
-    fontSize: 25,
-    textAlign: "center",
-    width: "100%",
+  claimStatus: {
+    marginRight: 20,
+    marginLeft: 10,
+    //marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "#2588cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  claimedStatus: {
+    marginRight: 20,
+    marginLeft: 10,
+    //marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "gray",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  fab: {
+    position: "absolute",
+    margin: 20,
+    right: 5,
+    bottom: 5,
+    backgroundColor: "#071b7a",
   },
   item: {
     flexDirection: "row",
     //padding: 20,
     //marginVertical: 8,
     //marginHorizontal: 16,
-    height: 60,
+    height: 70,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+  modalButton: {
+    borderRadius: 20,
+    padding: 10,
+    margin: 30,
+    elevation: 2,
+    position: "absolute",
+  },
+  modalHeader: {
+    fontSize: 25,
+    textAlign: "center",
+    margin: 10,
+  },
+  modalView: {
+    margin: 20,
+    height: 400,
+    width: 300,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  statusHeader: {
+    height: 70, // TODO: replace with relative positioning based on device
+    justifyContent: "center",
+    borderBottomWidth: 0.5,
+    borderTopWidth: 0.5,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#569dd1",
+    opacity: 0.8,
+  },
+  statusHeaderText: {
+    fontSize: 25,
+    textAlign: "center",
+    width: "100%",
+  },
+  statusText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  takenStatus: {
+    marginRight: 20,
+    marginLeft: 10,
+    //marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "red",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black",
   },
 });
-
 export default TasksPage;
