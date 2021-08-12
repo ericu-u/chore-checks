64ecf7f0d9f58ffe5c103fcf87302c0c86fc8e28
CrisostomoC3911 <38658629+CrisostomoC3911@users.noreply.github.com>
2021-08-12 13:56:48 -0700

    Profile and Household Selection
    Profile page styling done. Needs fonts
    Household selection almost done. Need to fix Join Household.

--- app/screens/HouseholdSelectionPage.js
+++ app/screens/HouseholdSelectionPage.js
@@ -1,18 +1,159 @@
-import React from 'react';
+import React, { useState } from "react";
+import Dialog from "react-native-dialog";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TouchableHighlight,
+} from "react-native";
 
 function HouseholdSelectionPage(props) {
-    return (
-        <ImageBackground
-            style={{flex:1}}
-            source={require("../assets/background-gradient.jpg")}
-        >
+  const handleJoin = () => console.log("Join Household");
+  //const handleCreate = () => console.log("Create Household");
+  const [visible, setVisible] = useState(false);
+  const [name, setName] = useState("");
+  var householdName = name;
+  const showDialogCreate = () => {
+    setVisible(true);
+  };
 
-        </ImageBackground>
-    );
+  const handleCancelCreate = () => {
+    setVisible(false);
+  };
+
+  const handleConfirmCreate = (text) => {
+    console.log(text);
+    setVisible(false);
+    navigation = useNavigation();
+    navigation.navigate("Profile");
+  };
+
+  return (
+    <ImageBackground
+      style={{ flex: 1 }}
+      source={require("../assets/background-gradient.jpg")}
+    >
+      <SafeAreaView style={styles.container}>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Image
+            source={require("../assets/householdSelect.png")}
+            style={styles.image}
+          />
+          <Text style={styles.titleText}>Would you like to...</Text>
+        </View>
+
+        <View
+          style={{
+            flex: 1.2,
+            flexDirection: "column",
+            alignItems: "space-evenly",
+          }}
+        >
+          <TouchableHighlight
+            style={styles.joinBackground}
+            onPress={handleJoin}
+          >
+            <View style={styles.container}>
+              <Text style={styles.buttonText}>
+                Join Existing {"\n"} Household
+              </Text>
+            </View>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={styles.createBackground}
+            onPress={showDialogCreate}
+          >
+            <View style={styles.container}>
+              <Text style={styles.buttonText}>Create New {"\n"} Household</Text>
+              <Dialog.Container visible={visible}>
+                <Dialog.Title>Enter Household Name</Dialog.Title>
+                <Dialog.Description>
+                  Please enter the full household name. This cannot be undone.
+                </Dialog.Description>
+                <Dialog.Input
+                  clearButtonMode="while-editing"
+                  clearTextOnFocus={true}
+                  maxLength={20}
+                  placeholder="Your Household Name"
+                  placeholderTextColor="#777"
+                  onChangeText={(householdName) => setName(householdName)}
+                  defaultValue={householdName}
+                ></Dialog.Input>
+                <Dialog.Button label="Cancel" onPress={handleCancelCreate} />
+                <Dialog.Button
+                  label="Confirm"
+                  onPress={() => handleConfirmCreate(householdName)}
+                />
+              </Dialog.Container>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "contain",
+    width: 230,
+    height: 230,
+  },
+  titleText: {
+    fontSize: 35,
+    color: "black",
+    textAlign: "center",
+    marginBottom: "16%",
+  },
+  text: {
+    fontSize: 20,
+  },
+  joinBackground: {
+    flex: 0.3,
+    backgroundColor: "#DAECF0",
+    borderRadius: 6,
+    borderColor: "black",
+    borderWidth: 1,
+    marginLeft: "5%",
+    marginRight: "2%",
+    marginBottom: "8%",
+    height: "100%",
+  },
+  createBackground: {
+    flex: 0.3,
+    backgroundColor: "#FFC681",
+    borderRadius: 6,
+    borderColor: "black",
+    borderWidth: 1,
+    marginLeft: "5%",
+    marginRight: "2%",
+    marginBottom: "4%",
+    height: "100%",
+  },
+  buttonText: {
+    marginLeft: "10%",
+    marginTop: "2%",
+    marginRight: "10%",
+    marginBottom: "2%",
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
-})
-
-export default HouseholdSelectionPage; 
+export default HouseholdSelectionPage;
