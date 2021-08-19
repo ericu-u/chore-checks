17ca0099fe8b57e11c4d206eec46d84830e3fd6d
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2021-08-19 11:42:51 -0700

    Sorting pog

--- app/screens/HouseholdPage.js
+++ app/screens/HouseholdPage.js
@@ -28,7 +28,7 @@ import {
 } from "@expo-google-fonts/montserrat";
 import SelectDropdown from "react-native-select-dropdown";
 
-const sortList = ["Completion Rate", "Tasks Completed", "Points"];
+const sortList = ["Points", "Completion Rate", "Tasks Completed"];
 
 var householdIDD = "hHeLFGtKHEHl6PPMwf9ek";
 export default class HouseholdPage extends React.Component {
@@ -100,7 +100,6 @@ export default class HouseholdPage extends React.Component {
     this.state.unsubscribe(); // We end the subscription here so we don't waste resources
   }
   render() {
-    // console.log(this.state.people);
     return (
       //replace all margins/paddings with relative positioning based on device
 
@@ -115,6 +114,7 @@ export default class HouseholdPage extends React.Component {
 
           {/* THE CRUCIAL DROP DOWN MENU FOR ACTUALLY SORTING*/}
           <SelectDropdown
+            defaultButtonText={"Points"}
             style={{ color: "red" }}
             data={sortList}
             onSelect={(selectedItem, index) => {
@@ -140,6 +140,10 @@ export default class HouseholdPage extends React.Component {
                     // console.log(doc.id, " => ", doc.data());
                     pain.push(doc.data());
                   });
+                  // this.setState({ tasks: pain });
+                  this.setState({ people: pain });
+
+                  // console.log(this.state.tasks);
                 })
                 .catch((error) => {
                   console.log("Error getting documents: ", error);
