17df41a763db2667f0d0c9170d238cb116b11b49
CrisostomoC3911 <38658629+CrisostomoC3911@users.noreply.github.com>
2021-08-24 09:55:52 -0700

    Update HouseholdSelectionPage.js

--- app/screens/HouseholdSelectionPage.js
+++ app/screens/HouseholdSelectionPage.js
@@ -18,28 +18,58 @@ import {
 import ProfilePage from "./ProfilePage";
 import AppLoading from "expo-app-loading";
 import { useFonts, Montserrat_500Medium } from "@expo-google-fonts/montserrat";
-
-function Changehouse({ navigation }) {
-  navigation = useNavigation();
-  return (
-    <Dialog.Button
-      label="Confirm"
-      onPress={() => navigation.navigate("ProfilePage")}
-    />
-  );
-}
-
 const Stack = createNativeStackNavigator();
 
-function HouseholdSelectionPage(navigation) {
-  let [fontsLoaded] = useFonts({
-    Montserrat_500Medium,
-  });
+const JoinHousehold = () => {
+  const [visible, setVisible] = useState(false);
+  const [code, setCode] = useState("");
+  const navigation = useNavigation();
+  var householdCode = code;
+  const showDialogJoin = () => {
+    setVisible(true);
+  };
 
-  const handleJoin = () => console.log("Join Household");
-  //const handleCreate = () => console.log("Create Household");
+  const handleCancelJoin = () => {
+    setVisible(false);
+  };
+
+  const handleConfirmJoin = () => {
+    console.log(householdCode);
+    setVisible(false);
+    navigation.navigate("Profile");
+  };
+
+  return (
+    <TouchableHighlight style={styles.joinBackground} onPress={showDialogJoin}>
+      <View style={styles.container}>
+        <Text style={styles.buttonText}>Join Existing {"\n"} Household</Text>
+        <Dialog.Container visible={visible}>
+          <Dialog.Title>Enter Household Code</Dialog.Title>
+          <Dialog.Description>
+            Please enter the full household code of an existing household.
+          </Dialog.Description>
+          <Dialog.Input
+            clearButtonMode="while-editing"
+            clearTextOnFocus={true}
+            maxLength={20}
+            placeholder="Your Household Code"
+            placeholderTextColor="#777"
+            onChangeText={(householdCode) => setCode(householdCode)}
+            defaultValue={householdCode}
+          ></Dialog.Input>
+          <Dialog.Button label="Cancel" onPress={handleCancelJoin} />
+          <Dialog.Button label="Confirm" onPress={handleConfirmJoin} />
+          {/*<Changehouse />*/}
+        </Dialog.Container>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const CreateHousehold = () => {
   const [visible, setVisible] = useState(false);
   const [name, setName] = useState("");
+  const navigation = useNavigation();
   var householdName = name;
   const showDialogCreate = () => {
     setVisible(true);
@@ -49,21 +79,54 @@ function HouseholdSelectionPage(navigation) {
     setVisible(false);
   };
 
-  const handleConfirmCreate = (text) => {
-    console.log(text);
+  const handleConfirmCreate = () => {
+    console.log(householdName);
     setVisible(false);
-    navigation = useNavigation({ ProfilePage });
-    navigation.navigate("ProfilePage");
+    navigation.navigate("Profile");
   };
 
+  return (
+    <TouchableHighlight
+      style={styles.createBackground}
+      onPress={showDialogCreate}
+    >
+      <View style={styles.container}>
+        <Text style={styles.buttonText}>Create New {"\n"} Household</Text>
+        <Dialog.Container visible={visible}>
+          <Dialog.Title>Enter Household Name</Dialog.Title>
+          <Dialog.Description>
+            Please enter the full household name. This cannot be undone.
+          </Dialog.Description>
+          <Dialog.Input
+            clearButtonMode="while-editing"
+            clearTextOnFocus={true}
+            maxLength={20}
+            placeholder="Your Household Name"
+            placeholderTextColor="#777"
+            onChangeText={(householdName) => setName(householdName)}
+            defaultValue={householdName}
+          ></Dialog.Input>
+          <Dialog.Button label="Cancel" onPress={handleCancelCreate} />
+          <Dialog.Button label="Confirm" onPress={handleConfirmCreate} />
+          {/*<Changehouse />*/}
+        </Dialog.Container>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+function HouseholdSelectionPage({ navigation }) {
+  let [fontsLoaded] = useFonts({
+    Montserrat_500Medium,
+  });
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
       (
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="ProfilePage">
-            <Stack.Screen name="ProfilePage" component={ProfilePage} />
+          <Stack.Navigator>
+            <Stack.Screen name="Profile" component={ProfilePage} />
           </Stack.Navigator>
         </NavigationContainer>
       ),
@@ -94,48 +157,8 @@ function HouseholdSelectionPage(navigation) {
                 justifyContent: "space-evenly",
               }}
             >
-              <TouchableHighlight
-                style={styles.joinBackground}
-                onPress={handleJoin}
-              >
-                <View style={styles.container}>
-                  <Text style={styles.buttonText}>
-                    Join Existing {"\n"} Household
-                  </Text>
-                </View>
-              </TouchableHighlight>
-
-              <TouchableHighlight
-                style={styles.createBackground}
-                onPress={showDialogCreate}
-              >
-                <View style={styles.container}>
-                  <Text style={styles.buttonText}>
-                    Create New {"\n"} Household
-                  </Text>
-                  <Dialog.Container visible={visible}>
-                    <Dialog.Title>Enter Household Name</Dialog.Title>
-                    <Dialog.Description>
-                      Please enter the full household name. This cannot be
-                      undone.
-                    </Dialog.Description>
-                    <Dialog.Input
-                      clearButtonMode="while-editing"
-                      clearTextOnFocus={true}
-                      maxLength={20}
-                      placeholder="Your Household Name"
-                      placeholderTextColor="#777"
-                      onChangeText={(householdName) => setName(householdName)}
-                      defaultValue={householdName}
-                    ></Dialog.Input>
-                    <Dialog.Button
-                      label="Cancel"
-                      onPress={handleCancelCreate}
-                    />
-                    <Changehouse />
-                  </Dialog.Container>
-                </View>
-              </TouchableHighlight>
+              <JoinHousehold />
+              <CreateHousehold />
             </View>
           </SafeAreaView>
         </ImageBackground>
